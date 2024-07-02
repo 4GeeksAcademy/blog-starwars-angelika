b79d3f9bab613c2e81d2b1b90a5d8c8441dafcfa
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-07-02 15:46:53 +0000

    ajustes favoritos y learn more

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { getImageUrl } from "../component/ImageMapping";
 
 export const Card = ({ item, addFavorite }) => {
-    const { name, gender, hair_color, eye_color, id } = item;
-    const imageUrl = getImageUrl(name);  
+    const { pathname } = useLocation();
+    console.log('PATH');
+    console.log(pathname);
+    const navigate = useNavigate();
+
+    const { name, gender, hair_color, eye_color } = item;
+    const imageUrl = getImageUrl(name);
+
+    const extractNumberFromUrl = (url) => {
+        const matches = url.match(/\/(\d+)\/$/);
+        if (matches) {
+            return matches[1];
+        }
+        return null;
+    };
+
+    const handleLearnMore = () => {
+        const num = extractNumberFromUrl(item.url);
+        navigate(`/detail-character/${num}`);
+    };
 
     return (
         <div className="card" style={{ width: '19rem', height: "650px" }}>
@@ -15,8 +33,8 @@ export const Card = ({ item, addFavorite }) => {
                 <p className="card-text">Hair Color: {hair_color}</p>
                 <p className="card-text">Eye Color: {eye_color}</p>
                 <div className="d-flex justify-content-between">
-                <Link to={`/detail-character/${id}`} className="btn btn-outline-primary">Learn More!</Link>                   
-                <button className="btn btn-outline-warning" onClick={() => addFavorite(item)}>
+                    <button className="btn btn-outline-primary" onClick={handleLearnMore}>Learn More!</button>
+                    <button className="btn btn-outline-warning" onClick={() => addFavorite(item)}>
                         <i className="fa fa-heart"></i>
                     </button>
                 </div>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,7 +4,7 @@ import { Context } from "../store/appContext";
 
 export const Navbar = () => {
     const { store, actions } = useContext(Context);
-    console.log("fav: "+store.favorites.name)
+    console.log("fav: "+JSON.stringify(store.favorites))
     return (
         <nav className="navbar navbar-light bg-light mb-3">
             <Link to="/">
@@ -13,20 +13,20 @@ export const Navbar = () => {
                 </span>
             </Link>
             <div className="ml-auto">
-                <div className="dropdown">
-                    <button className="btn btn-danger data-bs-toggle me-5" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div className="dropdown me-5">
+                    <button className="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Favorites <span className="badge badge-light">{store.favorites.length}</span>
                     </button>
-                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                        {store.favorites.map((fav, index) => (
-                            <div className="dropdown-item d-flex justify-content-between align-items-center" key={index}>
-                                <span>{fav.name}</span>
-                                <button className="btn btn-outline-danger btn-sm" onClick={() => actions.addFavorite(fav)}>
+                    <ul className="dropdown-menu w-100">
+                        {store.favorites.map((item, index) => (
+                            <li key={index}>
+                                {item.name}
+                                <button className="btn btn-outline-danger btn-sm" onClick={() => actions.addFavorite(item)}>
                                     <i className="fa fa-trash"></i>
                                 </button>
-                            </div>
+                            </li>
                         ))}
-                    </div>
+                    </ul>
                 </div>
             </div>
         </nav>
